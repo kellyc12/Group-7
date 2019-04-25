05b5d5c91a60ce720844ef5baa490d9134159344
Kan Yaan Tzi <kanyaantzi@gmail.com>
2019-04-25 19:02:43 -0400

    loads straight into login pags

--- routes/index.js
+++ routes/index.js
@@ -317,7 +317,7 @@ var getActivities =  function (items, list, callback){
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {title : 'Express'});
+  res.render('login');
 });
 
 
